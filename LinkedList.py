c7a3abd2d2dd6d6b8931f2307b4249a7933bfac7
Khurshedul Alam <92214287+Khurshedul51@users.noreply.github.com>
2024-01-20 19:44:06 +0600

    LinkedList class implementation
    I implemented a LinkedList class which consists of various methods

---
+++ LinkedList.py
@@ -0,0 +1,100 @@
+class Node:
+    def __init__(self, value, next_node=None) -> None:
+        self._data = value
+        self._next = next_node
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.__head = None
+        self.__tail = None
+        self.__no_of_nodes = 0
+    
+    def __len__(self) -> int:
+        return self.__no_of_nodes
+    
+    def __str__(self) -> str:
+        linked_list_str = ''
+        head_node = self.__head
+        if head_node != None:
+            current_node = head_node
+            while current_node != None:
+                arrow = ' -> ' if current_node._next != None else ''
+                linked_list_str = linked_list_str + str(current_node._data) + arrow
+                current_node = current_node._next
+            return f'[{linked_list_str}]'
+        else:
+            return '[]'
+
+    
+    def insert_head(self, value):
+        # new node
+        new_node = Node(value, next_node=self.__head)
+        # reassign head and tail
+        if self.__tail == None:
+            self.__tail = new_node
+        self.__head = new_node
+        # increment length of linked list by 1
+        self.__no_of_nodes += 1
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.is_empty():
+            self.__head = new_node
+            self.__tail = new_node
+        else:
+            self.__tail._next = new_node
+            self.__tail = new_node
+        self.__no_of_nodes += 1
+
+    def insert_after(self, after, value):
+        searched_node = self.search_node_by_value(after)
+        if searched_node != None:
+            if searched_node._next == None:
+                self.append(value)
+            else:
+                next_of_searched_node = searched_node._next
+                searched_node._next = Node(value, next_node=next_of_searched_node)
+            self.__no_of_nodes += 1
+        else:
+            raise ValueError('value of [arg: after] is not found')
+
+    def search_node_by_value(self, value) -> Node | None:
+        current_node = self.__head
+        while current_node != None:
+            if current_node._data == value:
+                return current_node
+            current_node = current_node._next
+        return None
+    
+    
+
+    def get_last_value(self):
+        return self.__tail._data
+    def get_last_node(self):
+        return self.__tail
+    
+    def get_first_value(self):
+        return self.__head._data
+    def get_first_node(self):
+        return self.__head
+    
+    def is_empty(self):
+        return True if self.__head == None else False
+    
+# ------------------------------------------------------------------------ #
+
+linked_list = LinkedList()
+# linked_list.insert_head(45)
+# linked_list.insert_head(67)
+# linked_list.insert_head(90)
+# print(linked_list.get_last_value())
+linked_list.append(12)
+linked_list.append('sakib')
+linked_list.insert_after(12, 'rakib')
+linked_list.insert_after('skib', 89)
+print(len(linked_list))
+print(linked_list)
+print(linked_list.get_first_value())
+print(linked_list.is_empty())
+
+print(linked_list.search_node_by_value('sakib'))
